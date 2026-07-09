c6308db6b07470fc306a6d3a9ba7f6eb790a9381
SPIDERN3MO <negisuyash1998@gmail.com>
2026-07-09 15:12:22 +0100

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] visited = new int[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int[] i:prerequisites){
+            List<Integer> list = map.getOrDefault(i[0], new ArrayList<>());
+            list.add(i[1]);
+            map.put(i[0], list);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!dfs(map, visited, i))   return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(Map<Integer, List<Integer>> map, int[] visited, int idx){
+        if(visited[idx] == 2)   return false;
+        if(visited[idx] == 1)   return true;
+        visited[idx] = 2;
+        boolean flag = true;
+        for(int i:map.getOrDefault(idx, new ArrayList<>())){
+            flag = flag && dfs(map, visited, i);
+        }
+        visited[idx] = 1;
+        return flag;
+    }
+}
